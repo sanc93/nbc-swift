c351f43095b06f9b09bdc07cc3dcadfd8a8c0f3c
sanc93 <sanc93@me.com>
2023-07-19 19:06:18 +0900

    calculator Lv.4

--- playgrounds/calculator.playground/Contents.swift
+++ playgrounds/calculator.playground/Contents.swift
@@ -4,43 +4,47 @@ import UIKit
 - Lv2 : Lv1에서 만든 Calculator 클래스에 나머지 연산을 가능하도록 코드를 추가하고, 연산 진행 후 출력하기
 - Lv3 : AddOperation(더하기), SubstractOperation(빼기), MultiplyOperation(곱하기), DivideOperation(나누기)
         연산 클래스를 만든 후 클래스간의 관계를 고려하여 Calculator 클래스와 관계를 맺기
+- Lv4 : AddOperation(더하기), SubtractOperation(빼기), MultiplyOperation(곱하기), DivideOperation(나누기)
+        연산 클래스들을 AbstractOperation라는 클래스명으로 만들어 사용하여 추상화하고 Calculator 클래스의 내부 코드를 변경합니다.
  */
 
-// 더하기 클래스
-class AddOperation {
+
+// 더하기 클래스(추상화 클래스)
+class AddOperation{
     func calculate(_ num1: Int, _ num2: Int) -> Int {
         return num1 + num2
    }
 }
 
 // 빼기 클래스
-class SubstractOperation {
-    func calculate(_ num1: Int, _ num2: Int) -> Int {
+class SubstractOperation: AddOperation {
+    override func calculate(_ num1: Int, _ num2: Int) -> Int {
         return num1 - num2
    }
 }
 
 // 곱하기 클래스
-class MultiplyOperation {
-    func calculate(_ num1: Int, _ num2: Int) -> Int {
+class MultiplyOperation: AddOperation {
+    override func calculate(_ num1: Int, _ num2: Int) -> Int {
         return num1 * num2
    }
 }
+
 // 나누기 클래스
-class DivideOperation {
-    func calculate(_ num1: Int, _ num2: Int) -> Int {
+class DivideOperation: AddOperation {
+    override func calculate(_ num1: Int, _ num2: Int) -> Int {
         return num1 / num2
    }
 }
 
 // 나머지 클래스
-class ModOperation {
-    func calculate(_ num1: Int, _ num2: Int) -> Int {
+class ModOperation: AddOperation {
+    override func calculate(_ num1: Int, _ num2: Int) -> Int {
         return num1 % num2
    }
 }
 
-// 계산기 클래스
+// 메인
 class Calculator {
     
     let add = AddOperation()
