587cbef21269811a8dfde96bd1c30667cb396704
sanc93 <sanc93@me.com>
2023-07-31 15:26:38 +0900

    iOS 앱개발 입문 1주차 1-2 실습

--- ColorApp/ColorApp/ViewController.swift
+++ ColorApp/ColorApp/ViewController.swift
@@ -15,5 +15,11 @@ class ViewController: UIViewController {
     }
 
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if true {
+            print("true if")
+        }
+    }
 }
 
