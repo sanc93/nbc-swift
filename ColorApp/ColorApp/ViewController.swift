06c59f6fd5279eeea31289049d52fe4794cbdad2
sanc93 <sanc93@me.com>
2023-07-31 19:17:31 +0900

    iOS 앱개발 입문 1주차 1-3 실습(1)

--- ColorApp/ColorApp/ViewController.swift
+++ ColorApp/ColorApp/ViewController.swift
@@ -11,15 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let myView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100)) // UIView 객체 생성
+        myView.backgroundColor = UIColor.yellow // 뷰 배경색 지정
+        self.view.addSubview(myView) // 뷰를 subView로 추가
+        myView.frame = CGRect(x: 50, y: 50, width: 200, height: 200) // 뷰의 위치 및 크기 변경
+        
     }
-
-
+    
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        if true {
-            print("true if")
-        }
+        super.viewWillAppear(animated) // 한번 호출 필요
+        print("viewWillAppear")
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+
 }
 
