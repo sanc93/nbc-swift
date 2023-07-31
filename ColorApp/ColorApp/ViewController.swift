66bd86829a737ae8c4f8d1f19d5fff24334f26c2
sanc93 <sanc93@me.com>
2023-07-31 20:16:37 +0900

    iOS 앱개발 입문 1주차 1-3 실습(2)

--- ColorApp/ColorApp/ViewController.swift
+++ ColorApp/ColorApp/ViewController.swift
@@ -11,12 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController = UINavigationController()
+        let viewController1 = UIViewController()
+        viewController1.title = "First View Controller"
+        navigationController?.setViewControllers([viewController1], animated: false)
+        addChild(navigationController!)
+        view.addSubview(navigationController!.view)
+        navigationController!.didMove(toParent: self)
         
-        let myView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100)) // UIView 객체 생성
-        myView.backgroundColor = UIColor.yellow // 뷰 배경색 지정
-        self.view.addSubview(myView) // 뷰를 subView로 추가
-        myView.frame = CGRect(x: 50, y: 50, width: 200, height: 200) // 뷰의 위치 및 크기 변경
+        navigationController!.view.frame = view.bounds
         
+        let button = UIButton(type: .system)
+        button.setTitle("Push View Controller", for: .normal)
+        button.addTarget(self, action: #selector(pushViewController), for: .touchUpInside)
+        navigationController?.navigationBar.addSubview(button)
+    }
+    
+    @objc func pushViewController() {
+        let newViewController = UIViewController()
+        newViewController.title = "New View Controller"
+        navigationController?.pushViewController(newViewController, animated: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
