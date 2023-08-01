b3efa013b890e66b40ba663b49d1f0f7ce592a9c
sanc93 <sanc93@me.com>
2023-08-01 20:56:03 +0900

    iOS 앱개발 입문 1주차 1-6 실습

--- ColorApp/ColorApp/ViewController.swift
+++ ColorApp/ColorApp/ViewController.swift
@@ -9,50 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var Button1: UIButton!
-    @IBAction func touchButton(_ sender: Any) {
-        self.view.backgroundColor = .yellow
-//        print("touch button")
-    }
     override func viewDidLoad() {
-//        Button1.setTitle("Button 1", for: .normal)
-        Button1.titleLabel?.textColor = .blue
         super.viewDidLoad()
-        let button = UIButton(type: .system)
-        button.setTitle("Push View Controller", for: .normal)
-        button.addTarget(self, action: #selector(pushViewController), for: .touchUpInside)
-        button.frame = CGRect(x: 200, y: 200, width: 200, height: 100)
+        
+        // UILabel
+        let label = UILabel(frame: CGRect(x: 100, y: 100, width: 300, height: 50))
+        label.text = "안녕하세요"
+        label.textColor = .green
+        label.font = .systemFont(ofSize: 30)
+        label.textAlignment = .center
+        self.view.addSubview(label)
+        
+        // UIImageView
+        let imageView = UIImageView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        let image = UIImage(systemName: "folder.fill")
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFit
+        self.view.addSubview(imageView)
+        
+        // UITextField
+        let textField = UITextField(frame: CGRect(x: 100, y: 100, width: 200, height: 60))
+        textField.placeholder = "이름을 입력해주세요"
+        self.view.addSubview(textField)
+        
+        // UIButton
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 50 ))
+        button.setTitle("버튼", for: .normal)
+        button.backgroundColor = .blue
+        button.addTarget(self, action: #selector(buttonAction2), for: .touchUpInside)
         self.view.addSubview(button)
+        
+        // UISwitch
+        let mySwitch = UISwitch(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        mySwitch.isOn = true
+        mySwitch.onTintColor = .green
+        mySwitch.thumbTintColor = .white
+        mySwitch.addTarget(self, action: #selector(switchToggled), for: .valueChanged)
+        self.view.addSubview(mySwitch)
     }
     
-    @objc func pushViewController() {
-        let newViewController = UIViewController()
-        newViewController.title = "New View Controller"
-        newViewController.view.backgroundColor = .white
-        navigationController?.pushViewController(newViewController, animated: true)
+    @objc func buttonAction2() {
+        print("버튼을 눌렀습니다.")
     }
     
-
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated) // 한번 호출 필요
-        print("viewWillAppear")
+    @objc func switchToggled() {
+        print("switch value changed")
+        
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("viewDidAppear")
-    }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("viewWillDisappear")
+    @IBAction func buttonAction(_ sender: Any) {
+        print("버튼 누르기")
     }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("viewDidDisappear")
+    @IBAction func sliderChange(_ sender: Any) {
+        print("slider", sender)
     }
-
+    @IBAction func segment(_ sender: Any) {
+        print("segment")
+    }
 }
 
