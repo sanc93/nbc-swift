b04f377c0378359639b42eddbb672f2a66327463
sanc93 <sanc93@me.com>
2023-08-02 23:39:16 +0900

    TODO Lv.1

--- TODO/TODO/ViewController.swift
+++ TODO/TODO/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toDoListBtn(_ sender: Any) {
+    }
+    
+    @IBAction func completedListBtn(_ sender: Any) {
+    }
 }
 
