e6158fff51ce1a562c59990db1782e1569d7daf5
sanc93 <sanc93@me.com>
2023-08-08 17:50:35 +0900

    ToDoTask 모델 생성

---
+++ TODO/TODO/Models/ToDoTask.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoTasks.swift
+//  TODO
+//
+//  Created by Sanghun K. on 2023/08/08.
+//
+
+import Foundation
+
+struct ToDoTask {
+    var id: Int
+    var inputText: String
+    var date: Date
+    var isCompleted: Bool
+}
