8acd33a6b62a8e1f437b4c4f4a5a618577ef0042
sanc93 <sanc93@me.com>
2023-08-08 20:32:00 +0900

    ToDoCustomCell 생성

---
+++ TODO/TODO/Views/ToDoCustomCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoCustomCell.swift
+//  TODO
+//
+//  Created by Sanghun K. on 2023/08/08.
+//
+
+import UIKit
+
+class ToDoCustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
