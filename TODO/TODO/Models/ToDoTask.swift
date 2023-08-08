64c884e946fd2db4525a2d366e99dc20aac75332
sanc93 <sanc93@me.com>
2023-08-08 18:55:28 +0900

    ToDoTask 구조체 id 타입을 UUID로

--- TODO/TODO/Models/ToDoTask.swift
+++ TODO/TODO/Models/ToDoTask.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct ToDoTask {
-    var id: Int
+    var id: UUID // UUID로 고유 ID값 생성. 향후 이 UUID값을 id값으로 사용해서 용이하게 데이터 추가/변경 가능
     var inputText: String
     var date: Date
     var isCompleted: Bool
