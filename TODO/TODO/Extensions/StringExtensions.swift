5235e8f0d6d43ba8187467ba7271347198b7017d
sanc93 <sanc93@me.com>
2023-08-18 18:12:58 +0900

    StringExtensions.swift 추가

---
+++ TODO/TODO/Extensions/StringExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  StringExtensions.swift
+//  TODO
+//
+//  Created by Sanghun K. on 2023/08/10.
+//
+import UIKit
+
+extension String {
+    func strikeThrough() -> NSAttributedString {
+        let attributeString = NSMutableAttributedString(string: self)
+        attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributeString.length))
+        return attributeString
+    }
+}
