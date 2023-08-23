32a08de8858a827d33e8e6d939bf72034356c8d0
sanc93 <sanc93@me.com>
2023-08-24 01:53:07 +0900

    ADD: SwiftLint 적용

--- HIEHJ/HIEHJ/Controllers/MainViewController.swift
+++ HIEHJ/HIEHJ/Controllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  HIEHJ
 //
 //  Created by Sanghun K. on 2023/08/23.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()  
+        
+        
     }
 
 
 }
-
---
+++ HIEHJ/HIEHJ/Controllers/ToDoListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoListViewController.swift
+//  HIEHJ
+//
+//  Created by Sanghun K. on 2023/08/24.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
